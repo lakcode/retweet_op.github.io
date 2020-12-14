887b17b17fb5e9667ae5ec1ef93b701c3cd5d9ff
Lakshmi Kenday <39344264+lakcode@users.noreply.github.com>
2020-12-14 13:41:06 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function loadDataset() {
 	// d3.json("data/debateD3.json", function(error, json) {
 		// data = json;
 
-	d3.json("data/Slang_retweet_v7.json", function(error, json) {
+	d3.json("data/Slang_retweet_Nov25.json", function(error, json) {
 		
 
 		data = processData(json);
